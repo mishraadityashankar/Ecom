4f4b501bbb18b9ec8db275d2fc68b5488a99066c
Shubham BHawsar <Bhawsarshubham741@gmail.com>
2019-07-30 19:04:00 +0530

    product models created

---
+++ modals/product_modal.js
@@ -0,0 +1,25 @@
+const mongoose =require('mongoose');
+const Schema=mongoose.Schema;
+
+
+
+const userSchema = new Schema({
+       _id:mongoose.Schema.Types.ObjectId,
+       pic:{type:String,required:true},
+       email:{type:String,required:true},
+       password:{
+           type:String, required:true
+       },
+       name:{type:String,required:true},
+       contact:{type:Number,required:true},
+       address:{type:String,required:true},
+       gender:{type:String,required:true},
+       age:{type:String,required:true},
+
+
+      
+
+   
+
+});
+module.exports=mongoose.model('User',userSchema);
